125c0e12d194df6806d6ad0f4ba74e4c17df4971
Pedro Henrique <phcpedro177@hotmail.com>
2023-04-07 14:11:39 -0300

    first commit

---
+++ src/structures/Client.js
@@ -0,0 +1,27 @@
+const constants = require("../util/constants");
+
+module.exports = class Client {
+  /**
+   *
+   * @param {import("../types").ClientConstructorParams} param0
+   */
+  constructor({ accessToken }) {
+    this.BASE_URL = constants.BASE_URL;
+    this.acessToken = accessToken;
+  }
+  /**
+   *
+   * @param {import("../types").ClientImagineParams} data
+   */
+  async imagine(data) {
+    const res = await fetch(this.BASE_URL, {
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`,
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+    const response = await res.blob();
+    return response;
+  }
+};
---
+++ src/types/index.d.ts
@@ -0,0 +1,9 @@
+export interface ClientConstructorParams {
+  accessToken: string;
+}
+
+export interface ClientImagineParams {
+  inputs: string;
+  width: number;
+  height: number;
+}
